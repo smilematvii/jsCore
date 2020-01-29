51c283ac5b8970ed0ed872b8acf6d32ba24b56b6
smilematvii <stoliaruk26071996@gmail.com>
2020-01-29 23:16:14 +0200

    домашка5 без 4 підтаска поки

---
+++ lesson4/hw4.js
@@ -0,0 +1,19 @@
+/* Task1 */
+
+let mentor = { 
+    course: "JS fundamental", 
+    duration: 3,
+    direction: "web-development" 
+};
+
+
+
+let num = 0;
+function propertyQuantity(){
+    for (num = 0; num <= arguments.length; num++){
+        return num;
+    }
+}
+
+propertyQuantity();
+
---
+++ lesson5/hw5.js
@@ -0,0 +1,55 @@
+/* Task1 */
+/*
+function testThrow(i){
+    try {
+               
+        throw exeption;
+           } catch(error) {
+            let result = i;
+            console.log(result);
+           }
+}
+
+testThrow(5);
+testThrow('Test');
+testThrow(new Error('An error happened'));
+*/
+/* Task2 */
+/*
+function calcRectangleArea(a, b){
+    try {
+        if(typeof arguments[0] !== "number" || typeof arguments[1] !== "number"){
+            throw exeption;
+        }else{
+            let res = a * b;
+            console.log(res);
+        }
+    }catch(exeption){
+        console.log("Error: Argument is NaN");
+    }
+}
+calcRectangleArea("f", 4);
+*/
+/* Task3 */
+/*
+function checkAge(){
+    try{
+        var age = prompt("How old are you?");
+        if(age >= 14){
+            alert("Have a nice film!");
+        }else{
+            throw error;
+        }
+    }catch(error){
+        if(age == ""){
+            alert("The field is empty! Please enter your age"); 
+        }else if(age < 14){
+            alert("You are too young!");
+        }else{
+            alert("The value is wrong! Please enter a number");
+        }
+
+checkAge();
+*/
+/* Task4 */
+
